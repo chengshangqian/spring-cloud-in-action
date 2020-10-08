b4d2f246e52562882a91fdd5e5e3c9cb52b321cc
chengshangqian <chengshangqian@126.com>
2020-10-08 21:34:54 +0800

    1、排除用户详情自动装配类UserDetailsServiceAutoConfiguration 2、增加本地开发配置文件application-dev.yaml，不需要启动配置中心占用开发资源

--- blog-service/src/main/java/com/fandou/coffee/learning/springcloud/blog/BlogServiceApp.java
+++ blog-service/src/main/java/com/fandou/coffee/learning/springcloud/blog/BlogServiceApp.java
@@ -2,12 +2,13 @@ package com.fandou.coffee.learning.springcloud.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class}) // 本地不做用户认证，排除用户详情服务自动装配
 @EnableEurekaClient // 开启Eureka客户端：注册和发现服务
 @EnableFeignClients // 开启Feign
 @EnableHystrix // 开启熔断器和熔断监控

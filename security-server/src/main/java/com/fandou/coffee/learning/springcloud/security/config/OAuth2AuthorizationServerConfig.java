adeb8e7798528e266dc806b20daab631bbf4ee89
chengshangqian <chengshangqian@126.com>
2020-10-09 18:30:44 +0800

    1、去掉了冗余的部分依赖和引用

--- security-server/src/main/java/com/fandou/coffee/learning/springcloud/security/config/OAuth2AuthorizationServerConfig.java
+++ security-server/src/main/java/com/fandou/coffee/learning/springcloud/security/config/OAuth2AuthorizationServerConfig.java
@@ -2,9 +2,7 @@ package com.fandou.coffee.learning.springcloud.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -14,10 +12,6 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-
-import javax.sql.DataSource;
 
 /**
  * 授权服务配置

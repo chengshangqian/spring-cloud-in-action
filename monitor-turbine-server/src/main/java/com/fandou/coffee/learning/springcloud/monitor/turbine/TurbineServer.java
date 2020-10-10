3bff282bcaf4e579148fefb0d12b1d27cdf8f448
chengshangqian <chengshangqian@126.com>
2020-10-10 17:16:35 +0800

    服务发现统一用@EnableDiscoveryClient注解

--- monitor-turbine-server/src/main/java/com/fandou/coffee/learning/springcloud/monitor/turbine/TurbineServer.java
+++ monitor-turbine-server/src/main/java/com/fandou/coffee/learning/springcloud/monitor/turbine/TurbineServer.java
@@ -2,13 +2,13 @@ package com.fandou.coffee.learning.springcloud.monitor.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableEurekaClient // 开启Eureka
+@EnableDiscoveryClient // 开启服务发现
 @EnableHystrix // 开启Hystrix及监控
 @EnableHystrixDashboard
 @EnableTurbine // 开启Turbine
